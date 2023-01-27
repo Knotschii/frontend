512d5c517f5b4104d82d384396a5b52f3479a25a
Knotschii <sian.lazari@gmail.com>
2023-01-27 16:22:04 +0100

    aktuell

---
+++ src/pages/Books.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+
+const Books = () => {
+    const [books, setBooks] = useState([])
+
+    useEffect(() => {
+        const fetchAllBooks = async () => {
+            try{
+                const res = await axios.get('http://localhost:8080/books/1')
+                console.log(res)
+            }catch(err){
+                console.log(err)
+            }
+        } 
+    },[])
+    
+  return (
+    <div>
+      Books test
+    </div>
+  )
+}
+
+export default Books
